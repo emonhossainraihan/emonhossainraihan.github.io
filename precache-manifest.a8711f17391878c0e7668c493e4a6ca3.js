e91363745901353c4c27b4963b993d8761ed4c6b
emonhossainraihan <mdemon7475@gmail.com>
2021-12-27 20:54:40 +0600

    :tada:

---
+++ precache-manifest.a8711f17391878c0e7668c493e4a6ca3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac48342013bbcd8896d411d35283ed19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55554e5bcecfcbdd236d",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "59316dd1139fc789c87b",
+    "url": "/static/js/2.4edf167a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4edf167a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55554e5bcecfcbdd236d",
+    "url": "/static/js/main.6e6cdc88.chunk.js"
+  },
+  {
+    "revision": "3f8b86c2880bacbb921e",
+    "url": "/static/js/runtime-main.a60d0284.js"
+  }
+]);
